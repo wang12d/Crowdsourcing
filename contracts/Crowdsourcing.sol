a4958dc487ca182bb1c7c253e3c40a9d2f4fed78
wang <awd1232009@gmail.com>
2020-10-30 20:17:27 +0800

    Add task awarding functionality

--- contracts/Crowdsourcing.sol
+++ contracts/Crowdsourcing.sol
@@ -10,20 +10,24 @@ contract Crowdsourcing {
     // 负责保存Workers所提交的押金，每一个地址都保存一定数量的押金
     mapping(address => uint) workerCollaterals;
     // 负责保存Requesters所提交的押金，每一个地址都对应一个Requesters,
-    // 上述Requesters和Workers所有的地址都需要Workers和Requesters在每个任务时
-    // 自动生成
+    // 上述Requesters和Workers所有的地址都需要Workers和Requesters
+    // 在每个任务时自动生成
     mapping(address => uint) requesterCollaterals;
     // 对于一个任务，剩下的Workers数量
     mapping(address => uint) remainingWorkers;
     // 对于每个任务所需要的押金
     mapping(address => uint) taskCollaterals;
+    // 计算每个Worker应该获得的奖励
+    mapping(address => uint) workerAwards;
+    // 确保每个任务都有其对应的workers，在进行奖励分发的时候使用
+    mapping(address => mapping(address => bool)) workersOfTask;
+    // 
     // 负责在特定条件下用来进行交互的Event，进行交易的触发
     // 假若Requester完成了任务并进行了评估，那么它就会触发一个交易来
     // 奖励Workers，同时Workers也可以通过监听该信息来判断其
     // 是否收到了相应的奖励
     event Tranfer(address indexed _from, address indexed _to, uint _val);
     event TaskPublished(address indexed _task, string description);
-    event MSGSender(address indexed);
     /**
      * 为了保证该Smart Contract能够接受来自其他用户的押金
      * 需要实现Fallback Function. 
@@ -33,20 +37,17 @@ contract Crowdsourcing {
      * 后期为了进行权限控制，需要判断消息发送者是否真的为Workers
      */
     function() external payable {
-        // string memory data = abi.decode(msg.data, (string));
-        string memory data = "requester";
-        string memory requester = "requester";
-        string memory worker = "worker";
-        require(keccak256(abi.encodePacked(data)) == keccak256(abi.encodePacked(requester)) || 
-            keccak256(abi.encodePacked(data)) == keccak256(abi.encodePacked(worker)), 
+        bytes memory requester = hex"01";
+        bytes memory worker = hex"00";
+        require(keccak256(abi.encodePacked(msg.data)) == keccak256(abi.encodePacked(requester)) || 
+            keccak256(abi.encodePacked(msg.data)) == keccak256(abi.encodePacked(worker)), 
             "Not supported data");
-        if (keccak256(abi.encodePacked(data)) == keccak256(abi.encodePacked(requester))) {
+        if (keccak256(abi.encodePacked(msg.data)) == keccak256(abi.encodePacked(requester))) {
             requesterCollaterals[msg.sender] += msg.value;
         }
         else {
             workerCollaterals[msg.sender] += msg.value;
         }
-        emit MSGSender(msg.sender);
     }
     // 
     // 创建一个新的众包任务，同时该函数会发出一个新的Event表示任务已经创建成功
@@ -59,6 +60,10 @@ contract Crowdsourcing {
         description = addString(description, addString("\nRewards: ", uintToString(exceptedRewards)));
         // 假若Requester已经提交了押金，那么其在智能合约里面存储的剩余
         // 押金应该是大于或等于collater的
+        requesterCollaterals[requester] -= collater;
+        // 目前这个状态也是处理对于任务奖励的金额
+        // 但是可能会有一个潜在的BUG，那就是在任务完成之后并没有
+        // 将其内容全部清零
         taskCollaterals[requester] = exceptedRewards;
         remainingWorkers[requester] = workers_needed;
         emit TaskPublished(requester, description);
@@ -75,20 +80,31 @@ contract Crowdsourcing {
     function JoinCrowdsourcingTask(address payable worker, address payable task) public {
         uint remWorkers = remainingWorkers[task];
         require (remWorkers > 0, "The task no longer needs more worker");
-        require (workerCollaterals[task] >= taskCollaterals[task], "Not enough amount to pay the collaterals");
+        require (workerCollaterals[worker] >= taskCollaterals[task], "Not enough amount to pay the collaterals");
+        workersOfTask[task][worker] = true;
+        workerAwards[worker] += taskCollaterals[task];
+        workerCollaterals[worker] -= taskCollaterals[task];
         // 此时任务算是被workers接受了
-        remainingWorkers[worker]--;
+        remainingWorkers[task]--;
     }
     /**
     * 进行任务奖励
     * 按照我们设计的协议，任务应该是由Workers直接提交到Requesters的，
     * 但是目前在这里作为测试用例，在智能合约上面实现该操作
     */
-    // function Rewarding(address payable worker, address payable requester, bool isSatisfy) public {
-    //     require(requester == msg.sender, "Only requester can trigger this message");
-    //     if (isSatisfy) {
-    //     }
-    // }
+    function Rewarding(address payable worker, bool isok) public {
+        // 只有Requester才能调用此信息
+        require(workersOfTask[msg.sender][worker] == true, "Only requester can call this to workers who participants its task");
+        if (isok) {
+            worker.transfer(workerAwards[worker]+taskCollaterals[msg.sender]);
+        }
+        else {
+            worker.transfer(workerAwards[worker]);
+        }
+        // 奖励已经发出，现在清除其状态
+        workersOfTask[msg.sender][worker] = false;
+        workerAwards[worker] = 0;
+    }
     // 辅助函数，用来进行从uint到string的转化，这一步主要是帮助Contracs进行任务的转化
     function uintToString(uint v) internal pure returns (string memory str) {
         uint maxlength = 100;
